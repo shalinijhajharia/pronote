a4ce286ae39913e8b163eea5e4f778880255ae1e
shalini jhajharia <shalinijhajharia185@gmail.com>
2021-12-18 19:17:14 +0530

    5665

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+import './components/Editor'
+import Edit from './components/Editor'
+
+function App() {
+  return (
+    <div className="App">
+<Edit/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Editor.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import "../App.css"
+import "./Editor.css"
+class Edit extends React.Component{
+
+    constructor(props) {
+        super(props);
+        // to bind the method with event handler without (). 
+        this.handleChange = this.handleChange.bind(this);
+
+        this.state =
+         { 
+            value: ''
+        };
+    }
+    handleChange(e){
+        this.setState({value: e.target.value});
+    }
+    render()
+    {
+        return(
+            <React.Fragment>
+                <div className="container">
+                <div className="input">
+                    <h3>Input</h3>
+                    <textarea className="input-text" onChange={this.handleChange} defaultValue={this.state.value}/>
+                </div>
+                <div className="output">
+                    <h3>Pro Note</h3>
+                    <div className="output-text">{this.state.value}</div>
+                </div>                
+            </div>
+            </React.Fragment>
+        )
+    }
+}
+export default Edit;
